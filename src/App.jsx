d85aef2034b22d001dcfa133a40ce83a5fae0a0b
Nini1912 <barbakadze.nino19@gmail.com>
2025-07-14 21:59:23 +0400

    add project files

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, useCallback } from "react";
+import "./App.css";
+
+function App() {
+  const [advice, setAdvice] = useState({
+    id: "",
+    advice: "",
+  });
+  const [loading, setLoading] = useState(true);
+
+  const newAdvice = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdvice({
+        id: data.slip.id,
+        advice: data.slip.advice,
+      });
+    } catch (error) {
+      console.error("Error fetching advice:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    newAdvice();
+  }, [newAdvice]);
+
+  return (
+    <div className="advice-container">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <span className="advice-number-heading">ADVICE #{advice.id}</span>
+          <p>&quot;{advice.advice}&quot;</p>
+        </>
+      )}
+      <div className="img-container">
+        <img src="/Group.svg" alt="" />
+      </div>
+      <button onClick={newAdvice} disabled={loading}>
+        <img src="/Shape.svg" alt="get new advice" />
+      </button>
+    </div>
+  );
+}
+
+export default App;
