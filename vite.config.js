da3ca0f414b0afbcf5f6f8413c9531ae9f9292b5
Nini1912 <137306287+Nini1912@users.noreply.github.com>
2025-07-14 22:21:30 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/random-advice-generator/',
+  base: "/random-advice-generator/",
+  plugins: [react()],
 });
